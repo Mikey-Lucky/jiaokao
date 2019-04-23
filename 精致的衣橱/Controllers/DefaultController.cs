787654ebd280a09be831eb5aee48cd48e077d64f
1833718739 <1833718739@qq.com>
2019-04-23 11:09:08 +0800

    ``

---
+++ 精致的衣橱/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 精致的衣橱.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
