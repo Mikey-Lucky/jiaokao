8f90b0621974c0ea8e4749d05226b75784857bf6
1833718739 <1833718739@qq.com>
2019-05-07 21:52:18 +0800

    lq

---
+++ DAL/SqlGoods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL;
+using Models;
+
+namespace DAL
+{
+    public class SqlGoods : IGoods
+    {
+        public IEnumerable<Goods> GetHotGoods()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Goods> GetNewGoods()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IDAL/IGoods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace IDAL
+{
+    public interface IGoods
+    {
+        //查找销量前几的商品
+        IEnumerable<Goods> GetHotGoods();
+        //查找最新上架商品
+        IEnumerable<Goods> GetNewGoods();
+    }
+}
