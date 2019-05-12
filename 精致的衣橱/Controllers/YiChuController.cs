4ff2d8ae5872c3d0ad1724e3d9694cfaa3931739
1833718739 <1833718739@qq.com>
2019-05-12 20:03:00 +0800

    首页初版

---
+++ 精致的衣橱/Controllers/YiChuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 精致的衣橱.Controllers
+{
+    public class YiChuController : Controller
+    {
+        // GET: YiChu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
