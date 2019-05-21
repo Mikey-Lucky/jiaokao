35238f75129d6a0fe187dc49dcd49e516277c5f5
1833718739 <1833718739@qq.com>
2019-05-21 19:32:59 +0800

    个人衣橱

--- BLL/NoteManager.cs
+++ BLL/NoteManager.cs
@@ -18,6 +18,28 @@ namespace BLL
             var hotnote = inote.Gethotnote(top);
             return hotnote;
         }
+        //通过用户id查询笔记
+        public IQueryable<Note> AllNoteByID(int id)
+        {
+            return inote.AllNoteByID(id);
+        }
+        //添加笔记
+        public void AddNote(Note note)
+        {
+            inote.AddNote(note);
+        }
+        //删除笔记
+        public bool DeleteNote(int id)
+        {
+            inote.DeleteNote(id);
+            return true;
+        }
+        //修改笔记
+        public void EditNote(Note note)
+        {
+            inote.EditNote(note);
+        }
+
 
     }
 }
--- 精致的衣橱/Controllers/YIChuShowController.cs
+++ 精致的衣橱/Controllers/YIChuShowController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
+using BLL;
+using 精致的衣橱.Models;
 
 namespace 精致的衣橱.Controllers
 {
     public class YIChuShowController : Controller
     {
+
+        NoteManager notes = new NoteManager();
+        YiChuShowViewModel ycsv = new YiChuShowViewModel();
         // GET: YIChuShow
         public ActionResult Index()
         {
@@ -16,6 +22,16 @@ namespace 精致的衣橱.Controllers
         public ActionResult Userhome()
         {
             return View();
+        }
+      
+        public ActionResult UserAllNote()
+        {
+            //通过用户id找说说
+            //var usershuoshuo = notes.AllNoteByID(Convert.ToInt32(Session["User_id"]));
+            //便于测试
+            var usershuoshuo = notes.AllNoteByID(1);
+            ycsv.AllNoteByID = usershuoshuo;
+            return View(ycsv);
         }
     }
 }
