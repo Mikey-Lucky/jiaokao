93d6f6a1d3becca2eb697a05b68cf019e9a83501
1833718739 <1833718739@qq.com>
2019-05-26 17:52:05 +0800

    秀米引入

---
+++ 精致的衣橱/Scripts/xuimi.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="E:\工作室\下\精致的衣橱\精致的衣橱\utf8-asp/third-party/xiumi-ue-dialog-v5.html" />
+/// <reference path="../utf8-asp/third-party/xss.min.js" />
+/// <reference path="E:\工作室\下\精致的衣橱\精致的衣橱\utf8-asp/third-party/xiumi-ue-dialog-v5.html" />
+/// <reference path="E:\工作室\下\精致的衣橱\精致的衣橱\utf8-asp/third-party/xiumi-ue-dialog-v5.html" />
+/**
+ * Created by shunchen_yang on 16/10/25.
+ */
+UE.registerUI('dialog', function (editor, uiName) {
+    var btn = new UE.ui.Button({
+        name: 'xiumi-connect',
+        title: 'keke',
+        onclick: function () {
+            var dialog = new UE.ui.Dialog({
+                iframeUrl: '../utf8-asp/third-party/xiumi-ue-dialog-v5.html',
+                editor: editor,
+                name: 'xiumi-connect',
+                title: "xiumi",
+                cssRules: "width: " + (window.innerWidth - 60) + "px;" + "height: " + (window.innerHeight - 60) + "px;",
+            });
+            dialog.render();
+            dialog.open();
+        }
+    });
+
+    return btn;
+});
