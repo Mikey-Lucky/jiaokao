2eb5b27e87af8a61d395583405ff085f4f24e5f2
吴俊琨 <@DESKTOP-GFPB4LC>
2019-06-03 11:45:05 +0800

    存储过程存在问题，返回类型有问题

--- DAL/SqlCart.cs
+++ DAL/SqlCart.cs
@@ -106,23 +106,12 @@ namespace DAL
             return shopcar;
         }
         //推荐，根据购物车中的衣服款式推荐
-        public IEnumerable<Goods> getgoodsbycartstyle(int userid)
+       public IEnumerable<Cart_Goods>  getgoodsbycart(int userid)
         {
-            var cart = db.Cart.Where(m => m.UserID == userid);
-            //var r = select * from Goods
-            //        where Season in(
-            //        select Season from Goods
-            //        where Season in (select season from Goods,Cart
-            //        where Goods.GoodsID = Cart.GoodsID and Cart.UserID = 7))
-           //var r = from a in db.Goods
-           //        where a.Type
-           //     (from a in db.Goods
-           //        from b in  cart
-           //        where a.GoodsID==b.GoodsID
-           //        select a.Type)
-                   
-
-
-        }
+            //var goods = db.Goods;
+            var t=db.Cart_Goods(userid);
+            return t;
+            
+       }
     }
 }
